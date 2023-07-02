2c13738870a33d1d3203a77a13dd26bed826a9bf
damywise <arifauliasutarman@gmail.com>
2023-07-02 22:50:29 +0700

    Update Beranda

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,138 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between">
+            <header className="flex flex-col w-full bg-white shadow-xl">
+                <div className="w-full h-8 bg-[#3e3e3e]"></div>
+                <nav className="p-4">
+                    <div className="flex flex-col w-full" id="top-bar">
+                        {/*top*/}
+                        <div className="flex flex-row mt-2 mb-6">
+                            <img src="/bangunpc.png" width={28} className="m-2 mr-8"/>
+                            <form action="/search" className="w-full mx-4">
+                                <div className="flex border border-gray-500 rounded-full">
+                                    <span className="m-auto p-3">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                             height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                             fill="none" stroke-linecap="round" stroke-linejoin="round"><path
+                                            stroke="none" d="M0 0h24v24H0z" fill="none"></path><path
+                                            d="M10 10m-7 0a7 7 0 1 0 14 0 7 7 0 1 0-14 0"></path><path
+                                            d="M21 21l-6-6"></path></svg>
+                                    </span>
+                                    <input type="text" className="w-full px-4 py-2"
+                                           placeholder="Cari PC-mu..."/>
+                                    <div className="border border-r-0 border-b-0 border-t-0 border-l-gray-500 p-3 px-6">
+                                        Kategori
+                                    </div>
+                                </div>
+                            </form>
+                            <div className="flex flex-row">
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                                <button
+                                    className="bg-blue-50 hover:bg-blue-200 active:bg-blue-300 transition-colors w-12 h-12 rounded-full mx-4">
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                         className="m-auto"
+                                         width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                         stroke="currentColor"
+                                         fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
+                                        <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+                                    </svg>
+                                </button>
+                                <button
+                                    className="bg-blue-50 hover:bg-blue-200 active:bg-blue-300 transition-colors w-12 h-12 rounded-full mx-4">
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                         className="m-auto"
+                                         width="24" height="24"
+                                         viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                         stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                                        <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                                        <path d="M17 17h-11v-14h-2"></path>
+                                        <path d="M6 5l14 1l-1 7h-13"></path>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                        {/*bottom*/}
+                        <div className="flex flex-wrap justify-between px-24">
+                            <button
+                                className="bg-blue-50 hover:bg-blue-200 active:bg-blue-300 transition-colors flex flex-row py-2 px-3 rounded-md">
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                     className="icon icon-tabler icon-tabler-grid-dots"
+                                     width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                     fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <path d="M5 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                                    <path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                                    <path d="M19 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                                    <path d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                                    <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                                    <path d="M19 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                                    <path d="M5 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                                    <path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                                    <path d="M19 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                                </svg>
+                                <span className="mx-5">
+                                Kategori
+                                </span>
+                            </button>
+                            <div className="flex flex-row justify-center">
+                                <button
+                                    className="bg-transparent hover:bg-orange-200 active:bg-orange-500 transition-colors flex flex-row py-2 px-3 mx-2 rounded-md">
+                                    Kategori
+                                </button>
+                                <button
+                                    className="bg-transparent hover:bg-orange-200 active:bg-orange-500 transition-colors flex flex-row py-2 px-3 mx-2 rounded-md">
+                                    Katalog
+                                </button>
+                                <button
+                                    className="bg-amber-500 hover:bg-amber-700 active:bg-orange-500 transition-colors flex flex-row py-2 px-3 mx-2 rounded-md text-white">
+                                    Simulasi Rakit PC
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <section>Content</section>
+            <section className="flex justify-around w-full bg-[#3e3e3e] pt-12 text-white">
+                <footer>
+                    <div className="flex md:flex-row flex-col px-12">
+                        <section className="flex flex-col w-full justify-start">
+                    <span className="font-bold">
+                        BangunPC
+                    </span>
+                            <br/>
+                            <span>
+                        Membantumu untuk merakit PC sesuai kebutuhan
+                    </span>
+                        </section>
+                        <section className="flex flex-col w-full justify-start md:my-0 my-12">
+                            <span className="font-bold">About Us</span>
+                            <br/>
+                            <a href="">Portfolio</a>
+                            <a href="">Careers</a>
+                            <a href="">Contact Us</a>
+                        </section>
+                        <section className="flex flex-col w-full justify-start">
+                            <span className="font-bold">Contact Us</span>
+                            <br/>
+                            Jl. Telekomunikasi. 1, Terusan Buahbatu - Bojongsoang, Telkom University, Sukapura, Kec.
+                            Dayeuhkolot, Kabupaten Bandung, Jawa Barat 40257
+                            <br/>
+                            +62 895-3430-86057
+                        </section>
+                    </div>
+                    <div className="my-12 border-t border-t-white"/>
+                    <section className="m-12 text-center" id="copyright">
+                        Copyright © 2023 BangunPC All Rights Reserved.
+                    </section>
+                </footer>
+            </section>
+        </main>
+    )
 }
