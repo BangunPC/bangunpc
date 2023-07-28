337f33fce2568ed8a9856eb6c2bff1ee11d921b0
A Arif A S <arifauliasutarman@gmail.com>
2023-07-28 13:22:35 +0700

    Update page.tsx

--- app/build/page.tsx
+++ app/build/page.tsx
@@ -1,22 +1,47 @@
 export default function Build() {
 
-    const titleTextClass = "font-extrabold text-lg uppercase"
+    const titleTextClass = "font-extrabold text-lg uppercase mb-2"
+
+    const games = [
+        'CALL OF DUTY\nMODERN\nWARFARE',
+        'FORTNITE',
+        'MINECRAFT',
+        'GRAND\nTHEFT\nAUTO V'
+    ]
 
     return (
         <>
             <center className="absolute flex z-50 w-screen h-screen backdrop-blur-md">
-                <section className="flex flex-col m-auto w-[32rem] h-[32rem] bg-slate-100 bg-opacity-80 rounded-lg p-6 shadow-xl">
+                <section className="flex flex-col m-auto w-[38rem] h-[32rem] bg-slate-100 bg-opacity-80 rounded-lg p-6 shadow-xl">
                     <header className="flex flex-col mb-4">
-                        <span className="font-semibold text-2xl">Build your PC</span>
+                        <span className="font-semibold text-4xl">Build your PC</span>
                         <span className="font-semibold">Select your chipset and budget</span>
                     </header>
                     <main className="flex flex-row justify-between">
-                        <section className="w-full mx-auto">
+                        <section className="w-full mx-auto bg-slate-200 rounded-lg p-4 shadow-sm">
                             <header className={titleTextClass}>
                                 performance
                             </header>
                             <main>
-                                [toggle button: game, office apps, render apps, IDEs, etc.]
+                                <div className="grid grid-cols-2 grid-rows-2 gap-4">
+
+                                    {games.map((text) => {
+                                        return (
+                                            <>
+                                                <div className="bg-white flex rounded-md aspect-square p-2">
+                                                    <span className="m-auto font-bold text-sm">
+                                                        {text}
+                                                    </span>
+                                                </div>
+                                            </>
+                                        )
+                                    })}
+                                </div>
+                                <div>
+                                    <span className="text-xs italic font-semibold">Resolution</span>
+                                    <br />
+                                    [HD|FHD|4K]
+                                </div>
                             </main>
                         </section>
                         <div className="flex flex-col items-center w-full mx-auto pt-3">
@@ -25,7 +50,7 @@ export default function Build() {
                                     chipset
                                 </header>
                                 <main>
-                                    [AMD or Intel]
+                                    [AMD|Intel]
                                 </main>
                             </section>
                             <section>
