88d8a82f8c90417dcfb82ecc292d97ea38b5e047
A Arif A S <arifauliasutarman@gmail.com>
2023-08-03 06:09:15 +0700

    update Next.js SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ const inter = Inter({ subsets: ['latin'] })
 const iconButtonClassname = "flex bg-blue-50 hover:bg-blue-200 active:bg-blue-300 active:scale-90 transition-all w-12 h-12 rounded-full mx-4"
 
 export const metadata: Metadata = {
-    title: 'BangunPC',
-    description: 'Semua kebutuhan PC Anda disini',
+    title: 'BangunPC — Find Parts, Components, Builds, and More',
+    description: 'Selamat datang di BangunPC, platform untuk penggemar PC! Kami ada untuk membantu Anda menemukan komponen PC dari platform belanja terpercaya. Manfaatkan fitur-fitur kami seperti simulasi rakit PC manual dan katalog komponen lengkap. Kami juga menyediakan panduan mudah untuk pemula agar Anda bisa merakit PC sendiri. Bergabunglah dengan BangunPC dan wujudkan PC impian Anda sekarang!',
     icons: [
         { rel: "apple-touch-icon", sizes: "180x180", url: "/apple-touch-icon.png" },
         { rel: "icon", sizes: "32x32", url: "/favicon-32x32.png" },
