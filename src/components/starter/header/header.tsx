7942ffba5028b92141183feca57d2441eb54d331
damywise <arifauliasutarman@gmail.com>
2023-09-27 07:18:21 +0700

    feat: better styling, responsive header menu, remove footer

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -7,30 +7,28 @@ import Devices from "../icons/devices";
 import User from "../icons/user";
 import DocumentText from "../icons/document-text";
 import Profile from "../icons/profile";
+import { TbMenu2 } from "@qwikest/icons/tablericons";
+
 
 const headersItems = [
   {
-    icon: <Devices width="24" height="24" class="inline-block fill-black" />,
+    icon: <Devices width="24" height="24" />,
     title: "Simulasi Rakit PC",
     href: "",
   },
   {
-    icon: <User width="24" height="24" class="inline-block fill-black" />,
+    icon: <User width="24" height="24" />,
     title: "Jasa Rakit PC",
     altTitle: "Jasa Servis PC",
     href: "",
   },
   {
-    icon: (
-      <ShoppingCart width="24" height="24" class="inline-block fill-black" />
-    ),
+    icon: <ShoppingCart width="24" height="24" />,
     title: "Katalog",
     href: "/catalogue",
   },
   {
-    icon: (
-      <DocumentText width="24" height="24" class="inline-block fill-black" />
-    ),
+    icon: <DocumentText width="24" height="24" />,
     title: "Blog",
     // href: "https://static-bangunpc.pages.dev/artikel/",
     href: "/blog",
@@ -41,30 +39,39 @@ export default component$(() => {
   return (
     <header class={styles.header}>
       {/* <div class={["container", styles.wrapper]}> */}
-      <div>
+      <div class={styles.logoAndToggle}>
         <a href="/" title="qwik">
           {/* <QwikLogo height={50} width={143} /> */}
-          <span class='text-2xl font-semibold text-black'>BangunPC</span>
+          <span class="text-2xl font-semibold text-black">BangunPC</span>
         </a>
+        <div>
+          <label for="toggle" class={styles.toggleButton}>
+            <TbMenu2 class='w-6' />
+          </label>
+        </div>
       </div>
-      <div class={styles.iconswrapper}>
-        {headersItems.map((item) => (
-          <Link
-            key={item.title}
-            href={item.href}
-            title={item.title}
-            class={styles.link}
-            // altTitle={item.altTitle}
-          >
-            {item.icon}
-            <span>{item.title}</span>
-          </Link>
-        ))}
+      <input type="checkbox" id="toggle" class={styles.toggle} />
+      <div class={styles.buttons}>
+        <div class={styles.iconswrapper}>
+          {headersItems.map((item) => (
+            <div key={item.title} class={styles.link}>
+              <Link
+                href={item.href}
+                title={item.title}
+                class={[styles.link, "transition duration-200"]}
+                // altTitle={item.altTitle}
+              >
+                {item.icon}
+                {item.title}
+              </Link>
+            </div>
+          ))}
+        </div>
+        <Link class={styles.login}>
+          <Profile width="24" height="24" class="inline-block fill-white" />
+          <span>Login</span>
+        </Link>
       </div>
-      <Link class={styles.login}>
-        <Profile width="24" height="24" class="inline-block fill-white" />
-        <span>Login</span>
-      </Link>
       {/* </div> */}
     </header>
   );
--- src/routes/blog/index.tsx
+++ src/routes/blog/index.tsx
@@ -1,42 +1,50 @@
 import { component$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
-import './blog.module.css';
+import styles from "./blog.module.css";
 
 export const useBlog = routeLoader$(async () => {
-  const modules = import.meta.glob('/src/blog/*.mdx', { eager: true })
+  const modules = import.meta.glob("/src/blog/*.mdx", { eager: true });
 
   const posts: any[] = [];
 
   for (const path in modules) {
     // @ts-ignore
     const post = modules[path].default().children.type();
-    posts.push(post)
+    posts.push(post);
   }
 
   return posts;
-})
+});
 
 export const useFrontmatter = routeLoader$(async () => {
-  const modules = import.meta.glob('/src/blog/*.mdx', { eager: true })
+  const modules = import.meta.glob("/src/blog/*.mdx", { eager: true });
 
   const posts: Post[] = [];
 
   for (const path in modules) {
     // @ts-ignore
-    const post = modules[path].frontmatter
-    posts.push(post)
+    const post = modules[path].frontmatter;
+    posts.push(post);
   }
 
   return posts;
-})
+});
 
 export default component$(() => {
   const data = useBlog();
   const meta = useFrontmatter();
   return (
-    <div class='max-w-[680px] mx-auto '>
-      {data.value.map((t, index) => <>
-        <h3 class='font-extrabold'>{meta.value[index].title}</h3> <br /> <h4>{meta.value[index].description}</h4> <h5>Oleh : {meta.value[index].authors}</h5> <div class='font-serif'>{t}</div></>)}
+    <div class="max-w-[680px] mx-4 md:mx-auto mt-8 ">
+      <span class={[styles.mdx]}>
+        {data.value.map((t, index) => (
+          <>
+            <h1 class="font-extrabold">{meta.value[index].title}</h1>
+            <h4>{meta.value[index].description}</h4> <br/>
+            <h5>Oleh : {meta.value[index].authors}</h5>
+            <div class="font-serif">{t}</div>
+          </>
+        ))}
+      </span>
     </div>
-  )
+  );
 });
