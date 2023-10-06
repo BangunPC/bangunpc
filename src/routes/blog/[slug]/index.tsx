93debbc9a63f7e0f47c03eade6a329e042e81da1
damywise <arifauliasutarman@gmail.com>
2023-10-06 19:45:54 +0700

    Revert "Adding Image and fix format"
    This reverts commit 1835f4c8d85d422dcd3e945f6736ac7e48e7800e.

--- src/routes/blog/[slug]/index.tsx
+++ src/routes/blog/[slug]/index.tsx
@@ -5,119 +5,120 @@ import styles from "./blog.module.css";
 import Profile from "~/components/starter/icons/profile";
 
 export const useBlog = routeLoader$(async () => {
-	const modules = import.meta.glob("/src/content/blogs/*.mdx", { eager: true });
+  const modules = import.meta.glob("/src/content/blogs/*.mdx", { eager: true });
 
-	const posts: Map<string, any> = new Map();
+  const posts: Map<string, any> = new Map();
 
-	for (const path in modules) {
-		// @ts-ignore
-		const post = modules[path].default().children.type();
+  for (const path in modules) {
+    // @ts-ignore
+    const post = modules[path].default().children.type();
 
-		const slug = path
-			.split("/")
-			.pop()
-			?.replace(/\.[^/.]+$/, ""); // Remove the file extension using regular expression
+    const slug = path
+      .split("/")
+      .pop()
+      ?.replace(/\.[^/.]+$/, ""); // Remove the file extension using regular expression
 
-		posts.set(slug!, post);
-	}
+    posts.set(slug!, post);
+  }
 
-	return posts;
+  return posts;
 });
 
 export const useFrontmatter = routeLoader$(async () => {
-	const modules = import.meta.glob("/src/content/blogs/*.mdx", { eager: true });
+  const modules = import.meta.glob("/src/content/blogs/*.mdx", { eager: true });
 
-	const posts: Map<string, any> = new Map();
+  const posts: Map<string, any> = new Map();
 
-	for (const path in modules) {
-		// @ts-ignore
-		const post = modules[path].frontmatter;
+  for (const path in modules) {
+    // @ts-ignore
+    const post = modules[path].frontmatter;
 
-		const slug = path
-			.split("/")
-			.pop()
-			?.replace(/\.[^/.]+$/, ""); // Remove the file extension using regular expression
+    const slug = path
+      .split("/")
+      .pop()
+      ?.replace(/\.[^/.]+$/, ""); // Remove the file extension using regular expression
 
-		posts.set(slug!, post);
-	}
+    posts.set(slug!, post);
+  }
 
-	return posts;
+  return posts;
 });
 
 export default component$(() => {
-	const data = useBlog();
-	const meta = useFrontmatter();
-	const slug = useLocation().params.slug;
+  const data = useBlog();
+  const meta = useFrontmatter();
+  const slug = useLocation().params.slug;
 
-	const metadata: Post = meta.value.get(slug);
+  const metadata: Post = meta.value.get(slug);
 
-	// TODO(damywise): Add image
-	const { title, description, created_at, categories, authors } = metadata;
-	const formattedDate = new Date(created_at).toLocaleDateString("id-ID", {
-		weekday: "long",
-		day: "numeric",
-		month: "long",
-		year: "numeric",
-	});
+  // TODO(damywise): Add image
+  const { title, description, created_at, categories, authors } =
+    metadata;
+  const formattedDate = new Date(created_at).toLocaleDateString("id-ID", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
 
-	return (
-		<div class='max-w-[680px] mx-4 md:mx-auto mt-8 '>
-			<article class={["prose md:prose-xl xl:prose-xl text-left", styles.mdx]}>
-				<>
-					<h1 class='font-extrabold text-left'>{title}</h1>
-					<h4>{description}</h4>
-					<div class='flex flex-row'>
-						{/* author profile circle */}
-						<Profile
-							width='48'
-							height='48'
-							class='w-12 h-12 mr-2 p-2 rounded-full border-2 my-auto'
-						/>
-						<div class={styles.metaout}>
-							<span>{authors}</span>
-							<div
-								class={[
-									styles.metain,
-									"flex-col sm:flex-row md:gap-1 leading-tight",
-								]}
-							>
-								<div class={styles.metacol}>
-									Published in
-									<span class='mx-1'>
-										{categories.map((category) => (
-											<a
-												key={category}
-												href={`/category/${category}`}
-												class='font-semibold'
-											>
-												{category}
-											</a>
-										))}
-									</span>
-								</div>
-								<div class={styles.metacol}>{formattedDate + " "}</div>
-							</div>
-						</div>
-					</div>
-					<br />
-					<div class='font-serif'>{data.value.get(slug)}</div>
-				</>
-			</article>
-		</div>
-	);
+  return (
+    <div class="max-w-[680px] mx-4 md:mx-auto mt-8 ">
+      <article class={["prose md:prose-xl xl:prose-xl text-left", styles.mdx]}>
+        <>
+          <h1 class="font-extrabold text-left">{title}</h1>
+          <h4>{description}</h4>
+          <div class="flex flex-row">
+            {/* author profile circle */}
+            <Profile
+              width="48"
+              height="48"
+              class="w-12 h-12 mr-2 p-2 rounded-full border-2 my-auto"
+            />
+            <div class={styles.metaout}>
+              <span>{authors}</span>
+              <div
+                class={[
+                  styles.metain,
+                  "flex-col sm:flex-row md:gap-1 leading-tight",
+                ]}
+              >
+                <div class={styles.metacol}>
+                  Published in
+                  <span class="mx-1">
+                    {categories.map((category) => (
+                      <a
+                        key={category}
+                        href={`/category/${category}`}
+                        class="font-semibold"
+                      >
+                        {category}
+                      </a>
+                    ))}
+                  </span>
+                </div>
+                <div class={styles.metacol}>{formattedDate + " "}</div>
+              </div>
+            </div>
+          </div>
+          <br />
+          <div class="font-serif">{data.value.get(slug)}</div>
+        </>
+      </article>
+    </div>
+  );
 });
 
 export const head: DocumentHead = ({ resolveValue, params }) => {
-	const slug = params.slug;
-	const metas = resolveValue(useFrontmatter);
-	const meta: Post = metas.get(slug);
-	return {
-		title: meta.title,
-		meta: [
-			{
-				name: "description",
-				content: meta.description,
-			},
-		],
-	};
+  const slug = params.slug;
+  const metas = resolveValue(useFrontmatter);
+  const meta: Post = metas.get(slug);
+  return {
+    title: meta.title,
+    meta: [
+      {
+        name: "description",
+        content: meta.description,
+      },
+    ],
+  };
 };
