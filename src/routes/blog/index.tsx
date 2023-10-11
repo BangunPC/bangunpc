8700eea64b21af7919f7f753ff1df85173fff84a
damywise <arifauliasutarman@gmail.com>
2023-10-11 19:30:58 +0700

    feat: blog meta title and description

--- src/routes/blog/index.tsx
+++ src/routes/blog/index.tsx
@@ -1,5 +1,5 @@
 import { component$ } from '@builder.io/qwik';
-import { Link, routeLoader$ } from '@builder.io/qwik-city';
+import { DocumentHead, Link, routeLoader$ } from '@builder.io/qwik-city';
 import styles from './posts.module.css';
 import Image1 from '/src/content/images/1.webp?jsx';
 import Image2 from '/src/content/images/2.webp?jsx';
@@ -65,3 +65,15 @@ export default component$(() => {
     </div>
   );
 });
+
+export const head: DocumentHead = {
+  title:
+    'BangunPC Tech Blog: Stay Updated on the Latest Trends, Reviews, and Tips',
+  meta: [
+    {
+      name: 'description',
+      content:
+        'Explore the world of PC technology with our comprehensive blog. Stay informed about the latest trends, read in-depth hardware reviews, and discover valuable tips and tricks. Enhance your gaming experience, optimize performance, and troubleshoot issues with expert insights and industry updates. ',
+    },
+  ],
+};
