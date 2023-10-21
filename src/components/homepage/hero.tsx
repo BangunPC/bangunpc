41f7f9d4c6ef1f16bef36c7120286a487db9f05d
A Arif A S <arifauliasutarman@gmail.com>
2023-10-21 19:00:20 +0700

    refactor: move homepage hero to its own component

---
+++ src/components/homepage/hero.tsx
@@ -0,0 +1,53 @@
+import { component$ } from "@builder.io/qwik";
+import Image from "~/content/images/hero-image.webp?jsx";
+import styles from "./hero.module.css";
+
+
+export default component$(() => {
+    return (
+        <div class={["bg-[#ECEEF5]", styles['hero-section']]}>
+            <div class={styles['hero-section-left']}>
+
+                <div class={styles['hero-section-left-top']}>
+
+                    <div class={styles['hero-buatpcsesuai']}>
+                        <span>
+                            Buat PC sesuai dengan kebutuhan dan gayamu.
+                        </span>
+                    </div>
+
+                    <div class={styles['hero-section-left-top-bottom']}>
+                        <span class={styles['hero-section-left-top-bottom-rakitpc']}>
+
+                            Rakit PC Impianmu
+                            <br />
+                            dengan<span class='text-[#4C5986]'> MUDAH </span>dan
+                            <br />
+                            <span class='text-[#4C5986]'>MURAH </span>
+
+                        </span>
+                        <span class={styles['hero-section-left-top-bottom-rekomendasi']}>
+                            Rekomendasi komponen yang berkualitas tinggi dengan harga terjangkau.
+                        </span>
+                    </div>
+                </div>
+                <div class={styles['hero-section-left-buttons']}>
+
+                    <btn class={styles['filled-button']}>
+                        <span>
+
+                            Rakit Sekarang
+                        </span>
+                    </btn>
+                    <btn class={styles['outlined-button']}>
+                        <span>
+
+                            Cari Komponen PC
+                        </span>
+                    </btn>
+                </div>
+            </div>
+            <Image class={styles["hero-section-right"]} />
+        </div>
+    )
+})
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,54 +1,11 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import Image from "~/content/images/hero-image.webp?jsx";
+import Hero from "~/components/homepage/hero";
 
 export default component$(() => {
   return (
     <>
-      <div class="bg-[#ECEEF5] hero-section">
-        <div class='hero-section-left'>
-
-          <div class='hero-section-left-top'>
-
-            <div class='hero-buatpcsesuai'>
-              <span>
-                Buat PC sesuai dengan kebutuhan dan gayamu.
-              </span>
-            </div>
-
-            <div class='hero-section-left-top-bottom'>
-              <span class='hero-section-left-top-bottom-rakitpc'>
-
-                Rakit PC Impianmu
-                <br />
-                dengan<span class='text-[#4C5986]'> MUDAH </span>dan
-                <br />
-                <span class='text-[#4C5986]'>MURAH </span>
-
-              </span>
-              <span class='hero-section-left-top-bottom-rekomendasi'>
-                Rekomendasi komponen yang berkualitas tinggi dengan harga terjangkau.
-              </span>
-            </div>
-          </div>
-          <div class='hero-section-left-buttons'>
-
-            <btn class='filled-button'>
-              <span>
-
-                Rakit Sekarang
-              </span>
-            </btn>
-            <btn class='outlined-button'>
-              <span>
-
-                Cari Komponen PC
-              </span>
-            </btn>
-          </div>
-        </div>
-        <Image class="hero-section-right" />
-      </div>
+      <Hero />
     </>
   );
 });
