d05d8be0145855f4bffb396909ad22039e57c59b
A Arif A S <arifauliasutarman@gmail.com>
2023-10-22 11:15:14 +0700

    feat(homepage): komponen

---
+++ src/components/homepage/komponen/komponen.tsx
@@ -0,0 +1,39 @@
+import { component$ } from "@builder.io/qwik";
+import styles from "./komponen.module.css";
+import { TbCheckbox } from "@qwikest/icons/tablericons";
+import Image from "~/content/images/homepage-komponen.webp?jsx";
+
+export default component$(() => {
+    return (
+        <div class={styles['komponen-section']}>
+            <div class={styles['komponen-image']}>
+                <Image />
+            </div>
+            <div class={styles['komponen-content-wrapper']}>
+
+                <div class={styles['komponen-content']}>
+                    <header class={styles['komponen-header']}>
+                        Bersama Kami, Wujudkan PC Idaman Anda dengan Mudah dan Profesional!
+                    </header>
+                    <main class={styles['komponen-main']}>
+                        Kami hadir untuk membuat pengalaman merakit PC menjadi mudah, tanpa ribet, dan profesional. Kami siap membantu Anda menemukan solusi sesuai dengan kebutuhan dan anggaran Anda, serta memberikan panduan dan dukungan yang dibutuhkan. Bersama kami, wujudkan PC impian Anda!
+                    </main>
+                    <div class={styles['komponen-checkbox']}>
+                        <TbCheckbox />
+                        <span>
+
+                            Simplenya Merakit PC
+                        </span>
+                    </div>
+                    <div class={styles['komponen-checkbox']}>
+                        <TbCheckbox />
+                        <span>
+
+                            Realisasi Impian Anda
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+})
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,13 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 import Banding from "~/components/homepage/banding/banding";
 import Hero from "~/components/homepage/hero/hero";
 import Jasa from "~/components/homepage/jasa/jasa";
+import Komponen from "~/components/homepage/komponen/komponen";
 
 export default component$(() => {
   return (
     <>
       <Hero />
+      <Komponen />
       <Jasa />
       <Banding />
     </>
