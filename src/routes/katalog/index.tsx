b986e599afe5906d0810a347ce86e5413a8aed34
A Arif A S <arifauliasutarman@gmail.com>
2023-11-09 20:33:47 +0700

    feat: katalog in header is toggle for modal instead of link
    - route /katalog redirects to home page for fallback

--- src/routes/katalog/index.tsx
+++ src/routes/katalog/index.tsx
@@ -1,11 +1,5 @@
-import { component$ } from "@builder.io/qwik";
-import { routeLoader$ } from "@builder.io/qwik-city";
-import { supabase } from "~/lib/db";
+import type { RequestEvent } from '@builder.io/qwik-city';
 
-export const useRecords = routeLoader$(async () => {
-    return await supabase.schema("product").from("v_all_product_list").select();
-  });
-
-export default component$(() => {
-    return <div>Halaman Katalog</div>;
-})
+export const onGet = async ({ redirect }: RequestEvent) => {
+  throw redirect(302, '/');
+};
