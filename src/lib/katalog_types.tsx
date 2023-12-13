4b1d8768d91eca6f0d8b88ffefffeca5af913947
A Arif A S <arifauliasutarman@gmail.com>
2023-12-13 22:06:47 +0700

    feat: remove redundant files, responsive katalog, search component

---
+++ src/lib/katalog_types.tsx
@@ -0,0 +1,137 @@
+import type { Database } from '~/lib/schema';
+
+
+export const casingHeaders = [
+    'Brand Name',
+    'Casing Type',
+    // 'Drive Bays',
+    'Fan Slots',
+    'GPU Max Length (mm)',
+]
+
+export const casingKeys = [
+    'brand_name',
+    'casing_type',
+    'fan_slots',
+    'max_gpu_length_mm',
+]
+
+export const cpuHeaders = [
+    'Total Core',
+    'Base Clock (GHz)',
+    'Max Clock (GHz)',
+    'Base Power (Watt)',
+    'Integrated GPU',
+]
+
+export const cpuKeys = [
+    'total_core',
+    'base_clock_ghz',
+    'max_clock_ghz',
+    'base_power_watt',
+    'integrated_gpu',
+]
+
+export const gpuHeaders = [
+    'Boost Clock (MHz)',
+    'Brand Name',
+    'Color',
+    'Core Clock (MHz)',
+    'Length (mm)',
+    'TDP (Watt)',
+    'VRAM (GB)'
+]  
+
+export const gpuKeys = [
+    'boost_clock_mhz',
+    'brand_name',
+    'chipset',
+    'base_clock_mhz',
+    'length_mm',
+    'tdp_watt',
+    'vram_gb',
+]
+
+export const memoryHeaders = [
+    'Amount',
+    'Brand Name',
+    'Capacity (GB)',
+    'Frequency (MHz)',
+    'Memory Type',
+]
+
+export const memoryKeys = [
+    'amount',
+    'brand_name',
+    'capacity_gb',
+    'frequency_mhz',
+    'memory_type',
+]
+
+export const motherboardHeaders = [
+    'Brand Name',
+    'Chipset',
+    'CPU Socket',
+    'Form Factor',
+    'Memory Max (GB)',
+    'Memory Type',
+]
+
+export const motherboardKeys = [
+    'brand_name',
+    'chipset',
+    'cpu_socket',
+    'form_factor',
+    'memory_max_gb',
+    'memory_type',
+]
+
+export const psuHeaders = [
+    'Brand Name',
+    'Efficiency Rating',
+    'Form Factor',
+    'Height (mm)',
+    'Modularity',
+    'Wattage',
+]
+
+export const psuKeys = [
+    'brand_name',
+    'efficiency_rating',
+    'form_factor',
+    'height_mm',
+    'modularity',
+    'wattage',
+]
+
+export const storageHeaders = [
+    'Brand Name',
+    'Type',
+    'Capacity (GB)',
+    'Category Name',
+    'Form Factor',
+    'Interface',
+]
+
+export const storageKeys = [
+    'brand_name',
+    'type',
+    'capacity_gb',
+    'category_name',
+    'form_factor',
+    'interface',
+  ];
+
+// export type All = { isMobile: boolean, all: Database['product']['Views']['v_all_products']['Row'] }
+
+// export type Casing = { isMobile: boolean, casing: Database['product']['Views']['v_casings']['Row'] }
+// export type Cpu = { isMobile: boolean, cpu: Database['product']['Views']['v_cpus']['Row'] }
+// export type Gpu = { isMobile: boolean, gpu: Database['product']['Views']['v_gpus']['Row'] }
+// export type Storage = { isMobile: boolean, storage: Database['product']['Views']['v_internal_storages']['Row'] }
+// export type Memory = { isMobile: boolean, memory: Database['product']['Views']['v_memories']['Row'] }
+// export type Motherboard = { isMobile: boolean, motherboard: Database['product']['Views']['v_motherboards']['Row'] }
+// export type Psu = { isMobile: boolean, psu: Database['product']['Views']['v_power_supplies']['Row'] }
+
+export type Details = { isMobile: boolean, detalis: Database['product']['Views']['v_product_details']['Row'] }
+
+export const productImageUrl = 'https://onawoodgnwkncueeyusr.supabase.co/storage/v1/object/public/product-images/'
--- src/routes/blog/index.tsx
+++ src/routes/blog/index.tsx
@@ -4,6 +4,7 @@ import { Link, routeLoader$ } from '@builder.io/qwik-city';
 import styles from './posts.module.css';
 import Image1 from '/src/content/images/1.webp?jsx';
 import Image2 from '/src/content/images/2.webp?jsx';
+import SearchBox from '~/components/common/search-box';
 
 const images = [<Image1 key={1} />, <Image2 key={2} />];
 
@@ -31,38 +32,45 @@ export const useFrontmatter = routeLoader$(async () => {
 export default component$(() => {
   const metas = useFrontmatter();
   return (
-    <div class={styles.wrap}>
-      {metas.value.map((meta, index) => (
-        <Link key={meta.slug} href={`/blog/${meta.slug}`} class={styles.card}>
-          <div class={styles.cardImage}>{images[index]}</div>
-          <span class="text-sm text-slate-600">
-            {new Date(meta.created_at).toLocaleDateString('id-ID', {
-              weekday: 'long',
-              day: 'numeric',
-              month: 'long',
-              year: 'numeric',
-            })}
-          </span>
-          <div class="flex flex-1 flex-col">
-            <span class="text-xl leading-[1.1] font-semibold">
-              {meta.title}
+    <div>
+      <div class="w-full max-w-7xl m-auto">
+        <div class="w-80 ml-auto mt-4 mr-4">
+          <SearchBox placeholder='Cari entri blog yang ingin kamu baca' />
+        </div>
+      </div>
+      <div class={styles.wrap}>
+        {metas.value.map((meta, index) => (
+          <Link key={meta.slug} href={`/blog/${meta.slug}`} class={styles.card}>
+            <div class={styles.cardImage}>{images[index]}</div>
+            <span class="text-sm text-slate-600">
+              {new Date(meta.created_at).toLocaleDateString('id-ID', {
+                weekday: 'long',
+                day: 'numeric',
+                month: 'long',
+                year: 'numeric',
+              })}
             </span>
-            <br />
-            <span class="text-slate-500">{meta.description}</span>
-          </div>
-          <div>
-            {/* <span>Oleh: {meta.authors.join(", ")}</span> */}
-            <div class={styles.tagsWrap}>
-              {meta.categories.map((tag) => (
-                <div key={tag} class={styles.tagWrapper}>
-                  <span class={styles.tag}>{tag}</span>
-                </div>
-              ))}
+            <div class="flex flex-1 flex-col">
+              <span class="text-xl leading-[1.1] font-semibold">
+                {meta.title}
+              </span>
+              <br />
+              <span class="text-slate-500">{meta.description}</span>
             </div>
-          </div>
-          <br />
-        </Link>
-      ))}
+            <div>
+              {/* <span>Oleh: {meta.authors.join(", ")}</span> */}
+              <div class={styles.tagsWrap}>
+                {meta.categories.map((tag) => (
+                  <div key={tag} class={styles.tagWrapper}>
+                    <span class={styles.tag}>{tag}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <br />
+          </Link>
+        ))}
+      </div>
     </div>
   );
 });
