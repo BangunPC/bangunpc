b526d34b6ce732c285bd0d978e70b862741a1cb0
A Arif A S <arifauliasutarman@gmail.com>
2024-01-15 13:52:11 +0700

    fix: clean

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { qwikReact } from '@builder.io/qwik-react/vite';
 
 export default defineConfig(() => {
   return {
