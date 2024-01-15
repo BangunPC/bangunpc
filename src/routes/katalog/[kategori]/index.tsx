1213420f0158245f506d972612815722197a3044
A Arif A S <arifauliasutarman@gmail.com>
2024-01-15 23:06:32 +0700

    feat: detail product add products comparison and affiliate link, sync schema type

--- src/routes/katalog/[kategori]/index.tsx
+++ src/routes/katalog/[kategori]/index.tsx
@@ -67,7 +67,7 @@ export const useRecords = routeLoader$(async (requestEvent) => {
         .eq('product_id', component['product_id'])
         .single()
 
-      if (imageData?.image_filenames.length == 0) {
+      if (imageData?.image_filenames?.length == 0) {
         imageUrls.push(undefined)
       }
       else {
