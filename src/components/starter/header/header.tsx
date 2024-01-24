8b4e4302fdb6547600a6b37d8dec205feedb865b
Irham Tri Ahmadi <irhamtria@gmail.com>
2024-01-24 23:30:37 +0700

    fix header padding

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -6,7 +6,7 @@ import ShoppingCart from "../icons/shopping-cart";
 import Devices from "../icons/devices";
 import User from "../icons/user";
 import DocumentText from "../icons/document-text";
-import Profile from "../icons/profile";
+// import Profile from "../icons/profile";
 import { TbMenu2 } from "@qwikest/icons/tablericons";
 
 
@@ -37,66 +37,68 @@ const headersItems = [
 
 export default component$(() => {
   return (
-    <header class={[styles.header, 'backdrop-blur-3xl']}>
-      {/* <div class={["container", styles.wrapper]}> */}
-      <div class={styles.logoAndToggle}>
-        <a href="/" title="qwik">
-          {/* <QwikLogo height={50} width={143} /> */}
-          <span class="text-2xl font-semibold text-black">Bangun PC</span>
-        </a>
-        <div>
-          <label for="toggle" class={styles.toggleButton}>
-            <TbMenu2 class='w-6 h-6' />
-          </label>
-        </div>
-      </div>
+    <>
       <input type="checkbox" id="toggle" class={styles.toggle} />
-      <div class={styles.buttons}>
-        <div class={[styles.iconswrapper, 'm-auto']}>
-          {headersItems.map((item) => (
-            <div key={item.title} class={styles.link}>
-              {item.disabled && (
-                <Link
-                  preventdefault:click
-                  title={item.title}
-                  class={[styles.link, "transition duration-200"]}
-                  onClick$={() => {
-                    // toast("Coming Soon!")
-                    alert('Coming Soon!')
-                  }}
-                >
-                  {item.icon}
-                  {item.title}
-                </Link>
-              )}
-              {item.href && (
-                <Link
-                  href={item.href}
-                  title={item.title}
-                  class={[styles.link, "transition duration-200"]}
-                >
-                  {item.icon}
-                  {item.title}
-                </Link>
-              )}
-              {item.labelFor && (
-
-                <label for={item.labelFor}
-                  class={[styles.link, "transition duration-200"]}
-                >
-                  {item.icon}
-                  {item.title}
-                </label>
-              )}
-            </div>
-          ))}
+      <header class={[styles.header, 'backdrop-blur-3xl pr-[20px]']}>
+        {/* <div class={["container", styles.wrapper]}> */}
+        <div class={[styles.logoAndToggle, 'mr-auto']}>
+          <Link href="/" title="qwik">
+            {/* <QwikLogo height={50} width={143} /> */}
+            <span class="text-2xl font-semibold text-black">Bangun PC</span>
+          </Link>
+          <div>
+            <label for="toggle" class={styles.toggleButton}>
+              <TbMenu2 class='w-6 h-6' />
+            </label>
+          </div>
         </div>
-        <Link class={styles.login}>
+        <div class={styles.buttons}>
+          <div class={[styles.iconswrapper, 'm-auto']}>
+            {headersItems.map((item) => (
+              <div key={item.title} class={styles.link}>
+                {item.disabled && (
+                  <Link
+                    preventdefault:click
+                    title={item.title}
+                    class={[styles.link, "transition duration-200"]}
+                    onClick$={() => {
+                      // toast("Coming Soon!")
+                      alert('Coming Soon!')
+                    }}
+                  >
+                    {item.icon}
+                    {item.title}
+                  </Link>
+                )}
+                {item.href && (
+                  <Link
+                    href={item.href}
+                    title={item.title}
+                    class={[styles.link, "transition duration-200"]}
+                  >
+                    {item.icon}
+                    {item.title}
+                  </Link>
+                )}
+                {item.labelFor && (
+
+                  <label for={item.labelFor}
+                    class={[styles.link, "transition duration-200"]}
+                  >
+                    {item.icon}
+                    {item.title}
+                  </label>
+                )}
+              </div>
+            ))}
+          </div>
+          {/* <Link class={styles.login}>
           <Profile width="24" height="24" class="inline-block fill-white" />
           <span>Login</span>
-        </Link>
-      </div>
-      {/* </div> */}
-    </header>
+        </Link> */}
+        </div>
+        {/* </div> */}
+      </header>
+    </>
   );
 });
