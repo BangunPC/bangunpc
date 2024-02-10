1406c6d63c6e8d8f86f053edb7ac588b64b49296
A Arif A S <arifauliasutarman@gmail.com>
2024-02-11 05:51:55 +0700

    fix: sitemap

--- adapters/cloudflare-pages/vite.config.ts
+++ adapters/cloudflare-pages/vite.config.ts
@@ -10,6 +10,6 @@ export default extendConfig(baseConfig, () => {
         input: ["src/entry.cloudflare-pages.tsx", "@qwik-city-plan"],
       },
     },
-    plugins: [cloudflarePagesAdapter()],
+    plugins: [cloudflarePagesAdapter({ssg: null})],
   };
 });
