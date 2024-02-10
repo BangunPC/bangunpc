731e4adf5dbdc9195c68937e4c8738d1cd023824
A Arif A S <arifauliasutarman@gmail.com>
2024-02-10 21:22:16 +0700

    fix(jasa card): layout

--- src/components/homepage/jasa/jasa-card/jasa-card.tsx
+++ src/components/homepage/jasa/jasa-card/jasa-card.tsx
@@ -12,32 +12,35 @@ export type JasaProps = {
 export default component$<JasaProps>((props) => {
     const borderClass = 'border border-black border-opacity-50';
     const arrow = <footer
-        class={`mx-auto rounded-full ${borderClass} w-fit px-6 text-2xl transition-colors bg-white hover:bg-[#4c5986]  hover:border-[#a6afce] hover:text-white cursor-pointer translate-y-[50%]`}
+        class={`mx-auto rounded-full ${borderClass} w-fit px-6 text-2xl transition-colors bg-white hover:bg-[#4c5986]  hover:border-[#a6afce] hover:text-white cursor-pointer translate-y-[-50%]`}
 
         onClick$={() => props.labelFor ? undefined : alert('Coming Soon!')}
     >
         <HiArrowUpSolid class="rotate-45" />
     </footer>
     return (
-        <div class={`min-w-fit max-w-sm px-4 bg-white rounded-lg my-8 shadow-xl shadow-slate-200 ${borderClass}`}>
-            <header class={[style['jasa-header'], 'mx-auto translate-y-[-50%]']}>
-                <div class={[style['jasa-icon'], style[`icon${props.iconNumber}`], 'mx-auto rounded-full justify-center items-center aspect-square w-14']}>
-                    <Slot name="icon" />
-                </div>
-            </header>
-            <main class={[style['jasacard-main'], 'flex flex-col justify-center text-center']}>
-                <span class={[style['jasacard-title'], 'w-full mx-auto leading-none mb-4']}>
-                    {props.title}
-                </span>
-                {props.description}
-            </main>
+        <div class='h-full py-8'>
+            <div class={`min-w-fit max-w-sm px-4 bg-white rounded-lg shadow-xl shadow-slate-200 h-full ${borderClass}`}>
+                <header class={[style['jasa-header'], 'mx-auto translate-y-[-50%]']}>
+                    <div class={[style['jasa-icon'], style[`icon${props.iconNumber}`], 'mx-auto rounded-full justify-center items-center aspect-square w-14']}>
+                        <Slot name="icon" />
+                    </div>
+                </header>
+                <main class={[style['jasacard-main'], 'flex flex-col justify-between text-center']}>
+                    <span class={[style['jasacard-title'], 'mx-auto leading-none mb-4']}>
+                        {props.title}
+                    </span>
+                    <span class='m-auto'>
+                        {props.description}
+                    </span>
+                </main>
+            </div>
             {props.labelFor && (
                 <label for={props.labelFor}>
                     {arrow}
                 </label>
             )}
             {!props.labelFor && arrow}
-
         </div>
     )
 })
