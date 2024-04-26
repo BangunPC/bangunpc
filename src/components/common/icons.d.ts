266e4597a800788d3276f8cc52a7e15b9e09a638
Ahmad A <25608913+damywise@users.noreply.github.com>
2024-04-26 07:38:46 +0700

    clean

--- src/components/common/icons.d.ts
+++
@@ -1,5 +0,0 @@
-export type IconProps = {
-  width?: string;
-  height?: string;
-  class?: string;
-};
--- src/lib/pagination.ts
+++
@@ -1,7 +0,0 @@
-export const getPagination = (page: number, size: number, count: number) => {
-    let from = (page - 1) * size;
-    if (page < 1 || from > count) from = 0;
-    const to = from + size;
-  
-    return { from, to };
-  };
