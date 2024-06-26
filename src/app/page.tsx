2428bf1d44c807afb395350ac8cc7c1c24c0ad64
damywise <25608913+damywise@users.noreply.github.com>
2024-06-27 05:59:39 +0700

    remove unused

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { ArrowUpRight } from "lucide-react";
+// import { ArrowUpRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "~/components/ui/button";
