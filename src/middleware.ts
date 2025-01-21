6d4412919613a2449885e151b70e65023cce1328
Irham Tri Ahmadi <91507189+irham3@users.noreply.github.com>
2025-01-22 05:27:16 +0700

    test: disable middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,19 @@
-import { corsMiddleware } from './middlewares/corsMiddleware'
-import { simulasiMiddleware } from './middlewares/simulasiMiddleware'
-import { NextRequest, NextResponse } from 'next/server'
+// import { corsMiddleware } from './middlewares/corsMiddleware'
+// import { simulasiMiddleware } from './middlewares/simulasiMiddleware'
+// import { NextRequest, NextResponse } from 'next/server'
 
-export async function middleware(req: NextRequest) {
-  // Apply the CORS middleware 
-  let response = corsMiddleware(req); 
+// export async function middleware(req: NextRequest) {
+//   // Apply the CORS middleware 
+//   let response = corsMiddleware(req); 
 
-  // Check if the request is for the specific path 
-  if (req.nextUrl.pathname.startsWith('/simulasi')) { 
-    // Apply the specific middleware and get the response 
-    response = simulasiMiddleware(req) || response; 
-  } 
-    return response || NextResponse.next();
-}
+//   // Check if the request is for the specific path 
+//   if (req.nextUrl.pathname.startsWith('/simulasi')) { 
+//     // Apply the specific middleware and get the response 
+//     response = simulasiMiddleware(req) || response; 
+//   } 
+//     return response || NextResponse.next();
+// }
 
-export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)']
-}
+// export const config = {
+//   matcher: ['/((?!_next/static|_next/image|favicon.ico).*)']
+// }
